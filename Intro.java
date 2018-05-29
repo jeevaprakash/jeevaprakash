20d51a9d6a2112586f099c23ad03ed2b3cf5db01
jeevaprakash <jevaprakash694@gmail.com>
2018-05-29 12:04:14 -0700

    Intro

---
+++ Intro.java
@@ -0,0 +1,8 @@
+public class Intro
+{
+public static void main(String a[])
+{
+ System.out.println("hi sir");
+ System.out.println ("im jeeva prakash");
+}
+}
--- datatype sample.java
+++
@@ -1,7 +0,0 @@
-public DataTypeSample
-{
-public static void main(string ln[])
-int i=12;
-byte j =13;
-system.out.println(i+j);
-{
